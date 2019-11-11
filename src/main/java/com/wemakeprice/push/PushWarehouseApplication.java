360b23003aa44b863f4a92c0344458b59acdb82d
이충일 <betterfly4202@gmail.com>
2019-11-11 22:17:27 +0900

    push-default settings

---
+++ src/main/java/com/wemakeprice/push/PushWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.wemakeprice.push;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushWarehouseApplication.class, args);
+	}
+
+}
