04596c9cd88c8616aa81ffb424f9093165d8b60c
betterfly <betterfly4202@gmail.com>
2020-08-02 21:01:28 +0900

    [feature] 코드리뷰 테스트 - 테스트 클래서 생성

---
+++ src/main/java/com/wemakeprice/push/item/NewClass.java
@@ -0,0 +1,13 @@
+package com.wemakeprice.push.item;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@RequiredArgsConstructor
+public class NewClass {
+    public void main(){
+        System.out.println("테스트 클래스 생성");
+    }
+}
